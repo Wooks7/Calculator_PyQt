0c8ae76eff97fd4aa01b005732b835b61a3365f6
Wooks7 <dragonze9797@naver.com>
2023-12-20 01:39:44 +0900

    #1 계산기 전체 main_layout의 row, column 수정
    1. layout_equation_solution 삭제 및 하나의 LineEdit으로 Equation과 Solution을 표시
    2. 'Main' 객체의 모든 'equation'을 'self.equation_solution'으로 대체

--- calculator_main.py
+++ calculator_main.py
@@ -12,20 +12,17 @@ class Main(QDialog):
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
         layout_operation = QGridLayout()
         layout_number = QGridLayout()
-        layout_equation_solution = QGridLayout()
         layout_addfeat = QGridLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Solution: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
+        self.equation_solution = QLineEdit("")  # 하나의 LineEdit으로 Equation과 Solution을 표시
+        self.equation_solution.setReadOnly(True)  # 읽기 전용 설정
+
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addWidget(label_equation, 0, 0)
-        layout_equation_solution.addWidget(self.equation, 0, 2)
-        layout_equation_solution.addWidget(label_solution, 1, 0)
-        layout_equation_solution.addWidget(self.solution, 1, 2)
+        layout_equation_solution = QHBoxLayout()  # Horizontal layout으로 변경
+        layout_equation_solution.addWidget(self.equation_solution)
+
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -98,11 +95,12 @@ class Main(QDialog):
         layout_addfeat.addWidget(button_square, 1, 1)
         layout_addfeat.addWidget(button_root, 1, 2)
 
-        ### 각 레이아웃을 main_layout 레이아웃에 추가
-        main_layout.addLayout(layout_equation_solution, 0, 0, 1, 0)
-        main_layout.addLayout(layout_operation, 1, 1)
-        main_layout.addLayout(layout_number, 1, 0)
-        main_layout.addLayout(layout_addfeat, 2, 0, 1, 0)
+        ## 각 레이아웃을 main_layout 레이아웃에 추가
+        main_layout.addLayout(layout_equation_solution, 0, 0, 1, 4)
+        main_layout.addLayout(layout_operation, 1, 3, 3, 1)
+        main_layout.addLayout(layout_number, 2, 0, 2, 3)
+        main_layout.addLayout(layout_addfeat, 1, 0, 1, 3)
+
 
         self.setLayout(main_layout)
         self.show()
@@ -111,28 +109,28 @@ class Main(QDialog):
     ### functions ###
     #################
     def number_button_clicked(self, num):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()  # LineEdit에 표시된 문자열 가져오기
         equation += str(num)
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)  # 변경된 문자열 설정  
 
     def button_operation_clicked(self, operation):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         equation += operation
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)
 
     def button_equal_clicked(self):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         solution = eval(equation)
-        self.solution.setText(str(solution))
+        self.equation_solution.setText(str(solution))
 
     def button_clear_clicked(self):
-        self.equation.setText("")
-        self.solution.setText("")
+        self.equation_solution.setText("")
+        self.equation_solution.setText("")
 
     def button_backspace_clicked(self):
-        equation = self.equation.text()
+        equation = self.equation_solution.text()
         equation = equation[:-1]
-        self.equation.setText(equation)
+        self.equation_solution.setText(equation)        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
